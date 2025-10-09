23271b1558a5dd8ade842bc8719ed555f236a1e0
user85-dev <user85@realtechltd.com>
2025-10-09 09:25:07 +0700

    feat(install): add nodemon

---
+++ install/05_nodemon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if ! command -v node &>/dev/null; then
+	echo "This env doesn't included nodejs"
+	exit 1
+fi
+
+if command -v nodemon &>/dev/null; then
+	echo "Nodemon is already installed"
+	exit 0
+fi
+
+npm -g nodemon
+
+echo "alias node-watch='nodemon --watch src --ext js,pug,json server.js'" >>"$HOME/.bash_aliases"
+
+echo "Nodemon installed successfully, and run with node-watch"
